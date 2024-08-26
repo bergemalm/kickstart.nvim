480acbe8dd1cc1b61d50595b739a19e34e47d21b
Mikael Bergemalm <mbergemalm@gmail.com>
2024-08-26 11:37:06 +0200

    feat: tune gopls

--- init.lua
+++ init.lua
@@ -570,7 +570,19 @@ require('lazy').setup({
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         -- clangd = {},
-        gopls = {},
+        gopls = {
+          -- cmd = { 'gopls' },
+          settings = {
+            gopls = {
+              analyses = {
+                unusedparams = true,
+                unusedvariable = true,
+              },
+              usePlaceholders = true,
+              staticcheck = true,
+            },
+          },
+        },
         -- pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
